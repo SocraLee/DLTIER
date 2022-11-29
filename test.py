5b5d378f44f3e054c3ed45f95c85672a3ebdd4c7
Yongkang Li <1800017764@pku.edu.cn>
2022-11-29 21:10:11 +0800

    Delete a file
    Delete test.py as it serves as it's just a program to verify the correctness of the code

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-from torchmetrics import F1Score
-target = torch.tensor([0, 1, 2, 0, 1, 2])
-preds = torch.tensor([0, 2, 1, 0, 0, 1])
-f1 = F1Score(num_classes=3,average='micro')
-print(type(f1(preds, target)))
